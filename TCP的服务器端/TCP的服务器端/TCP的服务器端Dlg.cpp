b61b79336d9ebaf3c4003ae31de6e7ef64ac84fa
张胜东 <435878393@qq.com>
2015-07-12 17:18:46 +0800

    初始

---
+++ TCP的服务器端/TCP的服务器端/TCP的服务器端Dlg.cpp
@@ -0,0 +1,93 @@
+
+// TCP的服务器端Dlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "TCP的服务器端.h"
+#include "TCP的服务器端Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTCP的服务器端Dlg 对话框
+
+
+
+CTCP的服务器端Dlg::CTCP的服务器端Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CTCP的服务器端Dlg::IDD, pParent)
+	, my_ServerIP(_T(""))
+	, my_ServerPort(0)
+	, my_ServerStatus(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CTCP的服务器端Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, my_ServerIP);
+	DDX_Text(pDX, IDC_EDIT2, my_ServerPort);
+	DDX_Text(pDX, IDC_EDIT3, my_ServerStatus);
+}
+
+BEGIN_MESSAGE_MAP(CTCP的服务器端Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CTCP的服务器端Dlg 消息处理程序
+
+BOOL CTCP的服务器端Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO:  在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CTCP的服务器端Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CTCP的服务器端Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
